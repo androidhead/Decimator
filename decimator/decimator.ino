0ebcc4a4e1c37e1b5ba2214afaa309d084787003
androidhead <androidboy@gmail.com>
2014-08-04 16:13:14 -0400

    Changed sample rate approach

--- decimator/decimator.ino
+++ decimator/decimator.ino
@@ -2,80 +2,89 @@
 // Based on rcarduino.blogspot.com previous work.
 // www.electrosmash.com/pedalshield
 
-int in_ADC0;  //variables for ADC value from ADC0 
-int POT0, POT1, POT2, out_DAC0; //variables for 3 pots (ADC8, ADC9, ADC10)
-int LED = 3;
-int FOOTSWITCH = 7; 
-int TOGGLE = 2; 
+ int in_ADC0, in_ADC1;  //variables for 2 ADCs values (ADC0, ADC1)
+ int POT0, POT1, POT2, out_DAC0, out_DAC1; //variables for 3 pots (ADC8, ADC9, ADC10)
+ int LED = 3;
+ int FOOTSWITCH = 7; 
+ int TOGGLE = 2; 
 
-//sample rate global vars
-int sampleRateFactor = 1;
-int oldSampleRateFactor = 1;
-int sampleCounter = 0;
-int oldSample;
- 
 void setup()
 {
+  /* turn on the timer clock in the power management controller */
+  pmc_set_writeprotect(false);
+  pmc_enable_periph_clk(ID_TC4);
+
+  /* we want wavesel 01 with RC */
+  TC_Configure(/* clock */TC1,/* channel */1, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC 
+  | TC_CMR_TCCLKS_TIMER_CLOCK2);
+  //set initial sample rate
+  TC_SetRC(TC1, 1, 238);
+  //TC_SetRC(TC1, 1, 109); // sets <>   96 Khz interrupt rate
+  
+  TC_Start(TC1, 1);
+ 
+  // enable timer interrupts on the timer
+  TC1->TC_CHANNEL[1].TC_IER=TC_IER_CPCS;
+  TC1->TC_CHANNEL[1].TC_IDR=~TC_IER_CPCS;
+ 
+  /* Enable the interrupt in the nested vector interrupt controller */
+  /* TC4_IRQn where 4 is the timer number * timer channels (3) + the channel number 
+  (=(1*3)+1) for timer1 channel1 */
+  NVIC_EnableIRQ(TC4_IRQn);
+   
   //ADC Configuration
   ADC->ADC_MR |= 0x80;   // DAC in free running mode.
   ADC->ADC_CR=2;         // Starts ADC conversion.
-  ADC->ADC_CHER=0x1CC0;  // Enable ADC channels 0,1,8,9,10).  
+  ADC->ADC_CHER=0x1CC0;    // Enable ADC channels 0 and 1.  
 
   //DAC Configuration
   analogWrite(DAC0,0);  // Enables DAC0
+  analogWrite(DAC1,0);  // Enables DAC1  
 }
-
 
 
 void loop()
 {
   //Read the ADCs
-  while((ADC->ADC_ISR & 0x1CC0)!=0x1CC0);// wait for ADC 0, 1, 8, 9, 10 conversion complete.
-  POT0=ADC->ADC_CDR[10];                 // read data from ADC8        
-  POT1=ADC->ADC_CDR[11];                 // read data from ADC9   
-  POT2=ADC->ADC_CDR[12];                 // read data from ADC10 
-  in_ADC0 = ADC->ADC_CDR[7];              // read data from ADC0
+  while((ADC->ADC_ISR & 0x1CC0)!=0x1CC0);  // wait for ADC 0, 1, 8, 9, 10 conversion complete.
+  in_ADC0=ADC->ADC_CDR[7];             // read data from ADC0
+  in_ADC1=ADC->ADC_CDR[6];             // read data from ADC1  
+
+
+  POT0=ADC->ADC_CDR[10];                   // read data from ADC8        
+  POT1=ADC->ADC_CDR[11];                   // read data from ADC9   
+  POT2=ADC->ADC_CDR[12];                   // read data from ADC10    
+}
+
+void TC4_Handler()
+{
+  // We need to get the status to clear it and allow the interrupt to fire again
+  TC_GetStatus(TC1, 1);
   
-  //sample rate
-  in_ADC0 = ChangeSampleRate(in_ADC0);               
-    
   //bit depth
-  in_ADC0 = ChangeBitDepth(in_ADC0, POT2);
+  in_ADC0 = ChangeBitDepth(in_ADC0, POT1);
+  in_ADC1 = ChangeBitDepth(in_ADC1, POT1);
+
+  ChangeSampleRate(POT0);
   
-      
-  //volume
-  out_DAC0=map(in_ADC0,0,4095,1,POT1);
-       
+  //Adjust the volume with POT2
+  out_DAC0=map(in_ADC0,0,4095,1,POT2);
+  out_DAC1=map(in_ADC1,0,4095,1,POT2);
+    
   //Write the DACs
-  dacc_set_channel_selection(DACC_INTERFACE, 0);       //select DAC channel 0
+  dacc_set_channel_selection(DACC_INTERFACE, 0);          //select DAC channel 0
   dacc_write_conversion_data(DACC_INTERFACE, out_DAC0);//write on DAC
+  dacc_set_channel_selection(DACC_INTERFACE, 1);          //select DAC channel 1
+  dacc_write_conversion_data(DACC_INTERFACE, out_DAC1);//write on DAC
 }
 
 
-int ChangeSampleRate(int sample)
+void ChangeSampleRate(int potPosition)
 {
-  //by default, return old sample
-  int output = oldSample;
-
-  // emulate lesser sample by only reading new sample a fraction of the time
-  // translate POT to sample rate factor
-  sampleRateFactor=map(POT0,0,4095,1,300);   
-  if(sampleCounter % sampleRateFactor == 0)  
-  {
-    output = sample;
-    oldSample = sample; //reset old sample
-  }
-  if(sampleCounter >= sampleRateFactor)
-  {
-    sampleCounter = 0;
-  }
-  else
-  {       
-    sampleCounter++;
-  }
-  
-  return output;
-}  
+  int sampleSize=map(potPosition,0,4095,109,8400);   
+  //10500/sample size = sampling rate in MHz
+  TC_SetRC(TC1, 1, sampleSize); 
+}
 
 int ChangeBitDepth(int input, int potPosition)
 {
@@ -113,4 +122,4 @@ int ChangeBitDepth(int input, int potPosition)
   input = map(input, 0, scaleTo, 0, 4095);
     
   return input;
-}  
+}
